1a5370f5f4754b5f57853b2cd7d627b655ff4d23
DressAnDy <mnhat2232004@gmail.com>
2026-02-16 22:52:08 +0700

    Implement Icon

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,20 @@
 /// <reference types="vite/client" />
 
+declare module '*.svg' {
+  const content: string
+  export default content
+}
+
+declare module '*.jpg' {
+  const content: string
+  export default content
+}
+
+declare module '*.png' {
+  const content: string
+  export default content
+}
+
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
   // Add other env variables as needed
